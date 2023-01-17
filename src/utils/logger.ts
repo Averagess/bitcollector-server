d713d2ee133cac43f83ba9ea47ff7ca62f0bda61
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-17 17:35:56 +0200

    added logging to file

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,23 @@
-import morgan from "morgan"
+import morgan from "morgan";
+import { createStream } from "rotating-file-stream";
 
-morgan.token("date", () => `[${new Date().toLocaleString()}]`)
-morgan.token("body", (req:any) => {
-  if(req.method === "GET") return ""
-  else return JSON.stringify(req.body)
-})
+morgan.token("date", () => `[${new Date().toLocaleString()}]`);
+morgan.token("body", (req: any) => {
+  if (req.method === "GET") return "";
+  else return JSON.stringify(req.body);
+});
 
-const logger = morgan(":date :remote-addr :method :url :status :res[content-length] - :response-time ms :body")
+const accessLogStream = createStream("access.log", {
+  interval: "1d",
+  path: "logs",
+  compress: "gzip",
+});
 
-export default logger
+const fileLogger = morgan(
+  ":date :remote-addr :method :url :status :res[content-length] - :response-time ms :body",
+  { stream: accessLogStream }
+);
+
+const consoleLogger = morgan(":date :remote-addr :method :url :status :res[content-length] - :response-time ms :body")
+
+export { consoleLogger, fileLogger };
