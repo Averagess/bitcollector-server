fdfdcc6e6f9319b64f6fa73e7ae4a1c947330269
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-17 17:51:03 +0200

    added new items

--- src/items.ts
+++ src/items.ts
@@ -3,23 +3,68 @@ import { Item } from "./models/player";
 const items: Item[] = [
   {
     name: "TRS-80 Model 1",
-    price: 10,
+    price: 50,
     cps: 1,
   }, 
   {
     name: "TRS-80 Model 2",
-    price: 20,
+    price: 100,
     cps: 2,
   },
   {
     name: "TRS-80 Model 4",
-    price: 50,
+    price: 500,
     cps: 10,
   },
   {
     name: "TRS-80 Model 4P",
-    price: 100,
+    price: 1000,
     cps: 20,
+  },
+  {
+    name: "Radio Shack Color Computer 1",
+    price: 5000,
+    cps: 100,
+  },
+  {
+    name: "Raspberry Pi",
+    price: 10000,
+    cps: 200,
+  },
+  {
+    name: "Raspberry Pi 2",
+    price: 50000,
+    cps: 1000,
+  },
+  {
+    name: "Raspberry Pi 3",
+    price: 100000,
+    cps: 2000,
+  },
+  {
+    name: "NVIDIA Jetson Nano",
+    price: 500000,
+    cps: 10000,
+  },
+  {
+    name: "NVIDIA Jetson Xavier NX",
+    price: 1000000,
+    cps: 20000,
+  },
+  {
+    name: "NVIDIA Geforce RTX 2080 Ti",
+    price: 5000000,
+    cps: 100000,
+  },
+  {
+    name: "NVIDIA Geforce RTX 3090",
+    price: 10000000,
+    cps: 200000,
+  },
+  {
+    name: "NVIDIA Tesla V100",
+    price: 500000000,
+    cps: 1000000,
   }
 ]
 
