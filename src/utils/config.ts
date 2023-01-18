8c80906d869b32319fb3671d5ece07a6b0d7693a
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-18 17:25:35 +0200

    added new config fields

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,10 +2,13 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 if(!process.env.MONGODB_URI) throw new Error("MONGODB_URI is not defined in .env file");
+else if(!process.env.MONGODB_TEST_URI) console.warn("MONGODB_TEST_URI is not defined in .env file. Tests will throw an error if you try to run them.")
 
 const config = {
   PORT: process.env.PORT || 3000,
-  MONGODB_URI: process.env.MONGODB_URI
+  ENVIRONMENT: process.env.NODE_ENV || 'development',
+  MONGODB_URI: process.env.MONGODB_URI,
+  MONGODB_TEST_URI: process.env.MONGODB_TEST_URI || null,
 }
 
 export default config;
