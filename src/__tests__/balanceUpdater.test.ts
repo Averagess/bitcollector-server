811d6df7800b6577f44b71627848900efaeda11e
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-20 01:29:43 +0200

    added tests for balanceUpdater

---
+++ src/__tests__/balanceUpdater.test.ts
@@ -0,0 +1,36 @@
+import balanceUpdater from "../helpers/balanceUpdater";
+
+describe("balanceUpdater", () => {
+  it("With cps 1, balance 0, should return balance as 1 after 1 second has passed", () => {
+    const oldBalance = BigInt(0);
+    const cps = BigInt(1);
+    const updatedAt = new Date(Date.now() - 1000)
+
+    const expected = BigInt(1)
+
+    const result = balanceUpdater({oldBalance, cps, updatedAt});  
+    expect(result).toBe(expected);
+  });
+
+  it("With cps 1000, balance 1250, should return 16250 after 15 secs have passed", () => {
+    const oldBalance = BigInt(1250)
+    const cps = BigInt(1000)
+    const updatedAt = new Date(Date.now() - 15000)
+
+    const expected = BigInt(16250)
+
+    const result = balanceUpdater({oldBalance, cps, updatedAt});
+    expect(result).toBe(expected);
+  })
+
+  it("With cps, Balance 0, should return 0 after 10 000 seconds have passed", () => {
+    const oldBalance = BigInt(0)
+    const cps = BigInt(0)
+    const updatedAt = new Date(Date.now() - 10_000_000)
+
+    const expected = BigInt(0)
+
+    const result = balanceUpdater({oldBalance, cps, updatedAt});
+    expect(result).toBe(expected);
+  })
+});
