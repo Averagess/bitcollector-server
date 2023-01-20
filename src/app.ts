4ab030d575da3234492224dc47ea8f1b06799687
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-20 06:47:17 +0200

    added an function that blocks outside connections

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Response, Request } from "express";
 
 import getRouter from "./routes/getters";
 import postRouter from "./routes/posters";
@@ -7,9 +7,17 @@ import { httpConsoleLogger, fileLogger } from "./utils/logger";
 
 const app = express();
 
+const blockOutsideConnections = (req: Request, res: Response, next: NextFunction) => {
+  if(req.ip !== "::ffff:127.0.0.1") return res.status(403).json({error: "Forbidden"}).end()
+
+  next()
+}
+
 app.use(express.json());
 app.use(httpConsoleLogger);
 app.use(fileLogger);
+
+app.use(blockOutsideConnections);
 
 app.use("/", getRouter);
 app.use("/", postRouter);
