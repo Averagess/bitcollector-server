093dff817a6dcf649a150feef8be2e7ecce81a31
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-20 05:15:00 +0200

    supports for float CPS now

--- src/helpers/balanceUpdater.ts
+++ src/helpers/balanceUpdater.ts
@@ -1,14 +1,17 @@
 interface Arguments {
   oldBalance: bigint;
-  cps: bigint;
+  cps: number;
   updatedAt: Date;
 }
 const balanceUpdater = ({ oldBalance, cps, updatedAt }: Arguments) => {
-  const secondsSinceLastUpdate = BigInt(
-    Math.floor((Date.now() - updatedAt.getTime()) / 1000)
-  );
+  const secondsSinceLastUpdate = Math.floor((Date.now() - updatedAt.getTime()) / 1000)
+  const newBalance = oldBalance + BigInt(Math.floor(cps) * secondsSinceLastUpdate)
 
-  return oldBalance + cps * secondsSinceLastUpdate;
+  if(newBalance > Number.MAX_SAFE_INTEGER) {
+    return newBalance
+  }
+  
+  return BigInt(Math.floor(Number(oldBalance) + cps * secondsSinceLastUpdate));
 };
 
 export default balanceUpdater;
