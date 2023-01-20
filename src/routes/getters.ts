73a5e50e1440f597c803f2d098b9d87816157d19
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-20 02:01:14 +0200

    code formatting

--- src/routes/getters.ts
+++ src/routes/getters.ts
@@ -75,8 +75,8 @@ router.get("/leaderboard", async (_req, res) => {
   res.send(leaderBoard);
 });
 
-if(process.env.NODE_ENV !== "test"){
-  cron.schedule("*/30 * * * *", updateLeaderboard)
+if (process.env.NODE_ENV !== "test") {
+  cron.schedule("*/30 * * * *", updateLeaderboard);
 }
 
 export default router;
