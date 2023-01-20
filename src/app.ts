fe05864cbe16cf39338bafd3ab93f69e95fd4e15
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-20 08:12:56 +0200

    fixed filter

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,15 @@ import express, { NextFunction, Response, Request } from "express";
 import getRouter from "./routes/getters";
 import postRouter from "./routes/posters";
 
-import { httpConsoleLogger, fileLogger } from "./utils/logger";
+import { httpConsoleLogger, fileLogger, logger } from "./utils/logger";
 
 const app = express();
 
 const blockOutsideConnections = (req: Request, res: Response, next: NextFunction) => {
-  if(req.ip !== "::ffff:127.0.0.1") return res.status(403).json({error: "Forbidden"}).end()
+  if(req.ip !== "::ffff:127.0.0.1" && req.ip !== "::1") {
+    logger.info(`Blocked request from outside connection, ip: ${req.ip}`)
+    return res.status(403).json({error: "Forbidden"}).end()
+  }
 
   next()
 }
