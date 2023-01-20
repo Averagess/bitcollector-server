e9d728b71d09627ac259be488352d4b6ed5f711a
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-20 05:14:00 +0200

    updated prices

--- src/items.ts
+++ src/items.ts
@@ -4,66 +4,66 @@ const items: Item[] = [
   {
     name: "TRS-80 Model 1",
     price: 50,
-    cps: 1,
+    cps: 0.1,
   }, 
   {
     name: "TRS-80 Model 2",
     price: 100,
-    cps: 2,
+    cps: 0.5,
   },
   {
     name: "TRS-80 Model 4",
-    price: 500,
-    cps: 10,
+    price: 300,
+    cps: 1,
   },
   {
     name: "TRS-80 Model 4P",
-    price: 1000,
-    cps: 20,
+    price: 500,
+    cps: 1.5,
   },
   {
     name: "Radio Shack Color Computer 1",
     price: 5000,
-    cps: 100,
+    cps: 2.5,
   },
   {
     name: "Raspberry Pi",
     price: 10000,
-    cps: 200,
+    cps: 5,
   },
   {
     name: "Raspberry Pi 2",
     price: 50000,
-    cps: 1000,
+    cps: 50,
   },
   {
     name: "Raspberry Pi 3",
     price: 100000,
-    cps: 2000,
+    cps: 100,
   },
   {
     name: "NVIDIA Jetson Nano",
     price: 500000,
-    cps: 10000,
+    cps: 500,
   },
   {
     name: "NVIDIA Jetson Xavier NX",
     price: 1000000,
-    cps: 20000,
+    cps: 1000,
   },
   {
     name: "NVIDIA Geforce RTX 2080 Ti",
     price: 5000000,
-    cps: 100000,
+    cps: 5000,
   },
   {
     name: "NVIDIA Geforce RTX 3090",
     price: 10000000,
-    cps: 200000,
+    cps: 100000,
   },
   {
     name: "NVIDIA Tesla V100",
-    price: 500000000,
+    price: 5000000000,
     cps: 1000000,
   }
 ]
