739db73e10cf72666d2e05969e1f3e8b2d6859d6
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-21 16:15:27 +0200

    removed filter

--- src/app.ts
+++ src/app.ts
@@ -7,20 +7,20 @@ import { httpConsoleLogger, fileLogger, logger } from "./utils/logger";
 
 const app = express();
 
-const blockOutsideConnections = (req: Request, res: Response, next: NextFunction) => {
-  if(req.ip !== "::ffff:127.0.0.1" && req.ip !== "::1") {
-    logger.info(`Blocked request from outside connection, ip: ${req.ip}`)
-    return res.status(403).json({error: "Forbidden"}).end()
-  }
+// const blockOutsideConnections = (req: Request, res: Response, next: NextFunction) => {
+//   if(req.ip !== "::ffff:127.0.0.1" && req.ip !== "::1") {
+//     logger.info(`Blocked request from outside connection, ip: ${req.ip}`)
+//     return res.status(403).json({error: "Forbidden"}).end()
+//   }
 
-  next()
-}
+//   next()
+// }
 
 app.use(express.json());
 app.use(httpConsoleLogger);
 app.use(fileLogger);
 
-app.use(blockOutsideConnections);
+// app.use(blockOutsideConnections);
 
 app.use("/", getRouter);
 app.use("/", postRouter);
