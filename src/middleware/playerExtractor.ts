cd0f842c3bd4766763ded5469a1ca9f098884806
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-21 23:35:20 +0200

    created playerExtractor middleware

---
+++ src/middleware/playerExtractor.ts
@@ -0,0 +1,22 @@
+import Player from "../models/player";
+import { NextFunction,  Response } from "express";
+import { ExtendedRequest } from "../types";
+
+const isString = (value: unknown): value is string => {
+  if(typeof value === "string" || value instanceof String) return true
+  return false
+}
+
+const playerExtractor = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+  const { discordId } = req.body
+
+  if(!discordId || !isString(discordId)) return res.status(400).json({ error: "Missing or invalid discordId" })
+
+  const player = await Player.findOne({ discordId })
+  if(!player) return res.status(404).json({ error: "Player not found" })
+
+  req.player = player
+  next()
+}
+
+export default playerExtractor;
