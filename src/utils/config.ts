c2512edce6b6e35393aec23aae2ec1b251c3cedf
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-23 02:38:53 +0200

    Renamed MONGODB_TEST_URI

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,13 +3,13 @@ import { logger } from './logger';
 dotenv.config();
 
 if(!process.env.MONGODB_URI) throw new Error("MONGODB_URI is not defined in .env file");
-else if(!process.env.MONGODB_TEST_URI) logger.warn("MONGODB_TEST_URI is not defined in .env file. Tests will throw an error if you try to run them.")
+else if(!process.env.MONGODB_DEV_URI && (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "dev")) logger.warn("MONGODB_DEV_URI is not defined in .env file. Tests will throw an error if you try to run them.")
 
 const config = {
   PORT: process.env.PORT || 3000,
   ENVIRONMENT: process.env.NODE_ENV,
   MONGODB_URI: process.env.MONGODB_URI,
-  MONGODB_TEST_URI: process.env.MONGODB_TEST_URI || null,
+  MONGODB_DEV_URI: process.env.MONGODB_DEV_URI || null,
 }
 
 export default config;
