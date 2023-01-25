1198a34586648390921598a1acef9aa70420e4e0
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-25 07:51:36 +0200

    Created randomItemDrop.ts Helper

---
+++ src/helpers/randomItemDrop.ts
@@ -0,0 +1,20 @@
+import items from "../items";
+import { Item } from "../models/player";
+
+function weightedRandom(min: number, max: number) {
+  return Math.round(max / (Math.random() * max + min));
+}
+
+/* 
+  Following function returns a random item from the items array.
+  The probability of an item being returned is based on the item's position in the array.
+  The first item in the array has the highest probability of being returned.
+  The last item in the array has the lowest probability of being returned.
+*/
+const randomItemDrop = (): Item => {
+  const randomIndex = weightedRandom(1, items.length - 1) - 1
+
+  return items[randomIndex];
+}
+console.log(randomItemDrop())
+export default randomItemDrop;
