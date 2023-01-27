5c87e3c6ceffcd9cea96331de5f0c6dd3672024a
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-27 13:46:52 +0200

    added cors, created login router, restructured api

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,25 @@
 import express from "express";
+import cors from "cors";
 
 import authenticator from "./middleware/authenticator";
+
 import getRouter from "./routes/getters";
 import postRouter from "./routes/posters";
+import putRouter from "./routes/putters"
+import loginRouter from "./routes/login";
 
 import { httpConsoleLogger, fileLogger } from "./utils/logger";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use(httpConsoleLogger);
 app.use(fileLogger);
+app.use("/login", loginRouter) // Login route is not protected by authenticator
 app.use(authenticator);
-app.use("/", getRouter);
-app.use("/", postRouter);
+app.use("/api", getRouter);
+app.use("/api", postRouter);
+app.use("/api", putRouter)
 
 export default app;
