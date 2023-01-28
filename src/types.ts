47eb24f3f395c9cecd73610b887baf183bf2a048
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-28 08:42:55 +0200

    moved all types into types.ts, updated imports

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,58 @@
 import { Request } from "express";
-import { PlayerInterface } from "./models/player";
-import { Document, Types } from "mongoose";
+import mongoose, { Document, Types } from "mongoose";
+import mongooseLong from 'mongoose-long'
+
+mongooseLong(mongoose)
+
+const Long = mongoose.Schema.Types.Long
+
+export interface Item {
+  name: string
+  price: number
+  cps: number
+}
+
+interface InventoryItem extends Item {
+  amount: number
+}
+export interface PlayerInterface {
+  discordDisplayName: string
+  discordId: string
+  balance: typeof Long | string
+  cps: number
+  inventory: InventoryItem[]
+  lastDaily: Date
+  dailyCount: number
+  unopenedCrates: number
+  openedCrates: number
+  blacklisted: { reason: string, started: Date }
+  blacklistHistory?: { reason: string, started: Date, ended: Date }[]
+  createdAt: Date
+  updatedAt: Date
+}
 
 export interface ExtendedRequest extends Request {
-  player?: Document<unknown, any, PlayerInterface> & PlayerInterface & {
-    _id: Types.ObjectId;
+  player?: Document<unknown, any, PlayerInterface> &
+    PlayerInterface & {
+      _id: Types.ObjectId;
+    };
 }
+
+export interface BalanceUpdaterArguments {
+  oldBalance: bigint;
+  cps: number;
+  updatedAt: Date;
+}
+
+export interface PlayerInLeaderboard {
+  discordDisplayName: string;
+  discordId: string;
+  cps: number;
+  balance: string;
+}
+
+export interface LeaderboardObject {
+  players: PlayerInLeaderboard[] | null;
+  createdAt: Date | null;
+  nextUpdate: Date | null;
 }
