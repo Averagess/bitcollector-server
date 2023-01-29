633fdd63c2af1367bc295b6a60a15edabb1f60f6
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-29 15:47:14 +0200

    sorted imports, improved leaderboard updator.

--- src/routes/getters.ts
+++ src/routes/getters.ts
@@ -1,13 +1,12 @@
-import { logger } from "../utils/logger";
 import { Router } from "express";
 const cron = require("node-cron");
 
+import balanceUpdater from "../helpers/balanceUpdater";
 import items from "../items";
 import Player from "../models/player";
-import balanceUpdater from "../helpers/balanceUpdater";
-import player from "../models/player";
+import { logger } from "../utils/logger";
 
-import { LeaderboardObject } from "../types"
+import { LeaderboardObject } from "../types";
 
 const router = Router();
 
@@ -20,26 +19,32 @@ const leaderBoard: LeaderboardObject = {
 const updateLeaderboard = async () => {
   logger.info("Updating leaderboard");
   const players = await Player.find({});
+
   const playersWithUpdatedBalance = players.map((player) => {
     const oldBalance = BigInt(player.balance as string);
     const cps = player.cps;
     const updatedAt = player.updatedAt;
 
-    const newBalance = balanceUpdater({ oldBalance, cps, updatedAt });
+    const newBalance = balanceUpdater({ oldBalance, cps, updatedAt }).toString();
+
+    const { discordDisplayName, discordId } = player;
 
     return {
-      discordDisplayName: player.discordDisplayName,
-      discordId: player.discordId,
-      cps: player.cps,
-      balance: newBalance.toString(),
+      discordDisplayName,
+      discordId,
+      cps, 
+      balance: newBalance
     };
-  });
+  }).sort((a,b) => {
+    const aBalance = BigInt(a.balance)
+    const bBalance = BigInt(b.balance)
 
-  const sortedPlayers = playersWithUpdatedBalance.sort(
-    (a, b) => Number(b.balance) - Number(a.balance)
-  );
+    if(aBalance > bBalance) return -1
+    else if(aBalance < bBalance) return 1
+    else return 0
+  })
 
-  leaderBoard.players = sortedPlayers.splice(0, 10);
+  leaderBoard.players = playersWithUpdatedBalance.splice(0, 10);
   leaderBoard.createdAt = new Date();
 
   const nextUpdate = new Date();
@@ -48,14 +53,19 @@ const updateLeaderboard = async () => {
     currentMinutes + 30 >= 60 && currentMinutes !== 60
       ? 0
       : 30; /* Basically this is either 00 (like 1:00) OR  30 (like 1:30) depending which time we are closer to at the execution time */
-  
-  const nextHourOrCurrentHour = next30or00minute === 0 ? nextUpdate.getHours() + 1 : nextUpdate.getHours();
-  
+
+  const nextHourOrCurrentHour =
+    next30or00minute === 0 ? nextUpdate.getHours() + 1 : nextUpdate.getHours();
+
   nextUpdate.setHours(nextHourOrCurrentHour);
   nextUpdate.setMinutes(next30or00minute);
 
   leaderBoard.nextUpdate = nextUpdate;
-  logger.info(`Leaderboard updated successfully, next update is ${nextUpdate.toLocaleString("fi-FI") }`);
+  logger.info(
+    `Leaderboard updated successfully, next update is ${nextUpdate.toLocaleString(
+      "fi-FI"
+    )}`
+  );
 };
 
 router.get("/allPlayers", async (_req, res) => {
@@ -76,11 +86,11 @@ router.get("/leaderboard", async (_req, res) => {
 });
 
 router.get("/blacklist", async (_req, res) => {
-  const players = await player.find()
-  
-  const blacklistedPlayers = players.filter(player => player.blacklisted)
-  res.send(blacklistedPlayers)
-})
+  const players = await Player.find();
+
+  const blacklistedPlayers = players.filter((player) => player.blacklisted);
+  res.send(blacklistedPlayers);
+});
 
 if (process.env.NODE_ENV !== "test") {
   cron.schedule("*/30 * * * *", updateLeaderboard);
@@ -91,28 +101,25 @@ router.get("/updateAllPlayers", async (_req, res) => {
   if (!players) return res.status(404).send("No players found");
 
   const updatedPlayers = await Promise.all(
-
     players.map(async (player) => {
-
       const secondsSinceLastUpdate = Math.floor(
         (Date.now() - new Date(player.updatedAt).getTime()) / 1000
       );
 
       if (!secondsSinceLastUpdate) return player;
-      
-      const oldBalance = BigInt(player.balance as string)
-      const cps = player.cps
-      const updatedAt = player.updatedAt
 
-      const newBalance = balanceUpdater({ oldBalance, cps, updatedAt})
+      const oldBalance = BigInt(player.balance as string);
+      const cps = player.cps;
+      const updatedAt = player.updatedAt;
+
+      const newBalance = balanceUpdater({ oldBalance, cps, updatedAt });
 
       logger.info(`Updating player ${player.discordId}
       player.balance (old): ${player.balance}
       player.balance (new): ${newBalance}
       balance diff: ${BigInt(newBalance) - BigInt(player.balance as string)}
       secondsSinceLastUpdate: ${secondsSinceLastUpdate}
-      player.cps: ${player.cps}`
-      );
+      player.cps: ${player.cps}`);
 
       player.balance = newBalance.toString();
 
