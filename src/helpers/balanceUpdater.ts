24b34568cdc67688a1e5010a5cdd73bde12e4026
Averagess <45748163+Averagess@users.noreply.github.com>
2023-01-29 15:45:29 +0200

    added comments

--- src/helpers/balanceUpdater.ts
+++ src/helpers/balanceUpdater.ts
@@ -4,11 +4,11 @@ const balanceUpdater = ({ oldBalance, cps, updatedAt }: BalanceUpdaterArguments)
   const secondsSinceLastUpdate = Math.floor((Date.now() - updatedAt.getTime()) / 1000)
   const newBalance = oldBalance + BigInt(Math.floor(cps) * secondsSinceLastUpdate)
 
-  if(newBalance > Number.MAX_SAFE_INTEGER) {
-    return newBalance
-  }
-  
-  return BigInt(Math.floor(Number(oldBalance) + cps * secondsSinceLastUpdate));
+
+  // Unaccurate, doesnt take in account the decimal part of cps. But players balance is so big that it doesnt matter.
+  if(newBalance > Number.MAX_SAFE_INTEGER) return newBalance
+  // Accurate, takes in account the decimal part of cps too.
+  else return BigInt(Math.floor(Number(oldBalance) + cps * secondsSinceLastUpdate));
 };
 
 export default balanceUpdater;
