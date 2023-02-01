f391f65ed7590f2153fd8f4be5aef50aad90ed84
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-01 14:20:31 +0200

    optimized

--- src/routes/putters.ts
+++ src/routes/putters.ts
@@ -1,17 +1,16 @@
-import player from "../models/player";
+import Player from "../models/player";
 import { Router } from "express";
 const router = Router()
-
 
 router.put("/updatePlayer", async (req,res) => {
   const body = req.body;
 
-  try {
-    await new player(body).validate()
-    const findPlayer = await player.findOneAndUpdate({discordId: body.discordId}, body, {new: true, upsert: false})
+  if(!body.discordId || Object.keys(body).length === 0) return res.status(400).json({error: "Missing or invalid body"})
 
-    if(findPlayer === null) return res.status(404).json({error: "Player not found"})
-    else return res.status(200).json(findPlayer)
+  try {
+  const player = await Player.findOneAndUpdate({discordId: body.discordId}, body, {new: true})
+  if(!player) return res.status(404).json({error: "Player not found"})
+  else return res.status(200).json({message: "Player updated"})
   } catch (error) {
     if(error instanceof Error){
       return res.status(400).json({error: error.message})
