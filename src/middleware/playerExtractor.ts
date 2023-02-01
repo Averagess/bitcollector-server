c7ad11e59e8077ff346190e1f766548a7c499119
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-01 14:17:39 +0200

    removed unneeded check from if statement

--- src/middleware/playerExtractor.ts
+++ src/middleware/playerExtractor.ts
@@ -6,7 +6,7 @@ import { isString } from "../utils/isString";
 const playerExtractor = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
   const { discordId } = req.body
 
-  if(!discordId || !isString(discordId)) return res.status(400).json({ error: "Missing or invalid discordId" })
+  if(!isString(discordId)) return res.status(400).json({ error: "Missing or invalid discordId" })
 
   const player = await Player.findOne({ discordId })
   if(!player) return res.status(404).json({ error: "Player not found" })
