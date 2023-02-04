faf89e85845427fa90896bfeb1fcc9ea51a2c0f3
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-04 16:51:54 +0200

    sorted imports

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-import { MONGODB_URI, ENVIRONMENT, MONGODB_DEV_URI, CI } from "./config";
 
+import { MONGODB_URI, ENVIRONMENT, MONGODB_DEV_URI, CI } from "./config";
 import { logger } from "./logger";
 
 mongoose.set("strictQuery", false);
