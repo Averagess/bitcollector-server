9b98b58e08e0b088e537c09c503dd835d4427c6c
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-04 15:02:52 +0200

    removed unneeded ENVIRONMENT check

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -9,28 +9,19 @@ const connectToDatabase = async () => {
   try {
     logger.info("Attempting connecting to database");
     if (CI === "true" || (typeof CI === "boolean" && CI)) {
-      logger.info("Connecting to local database because CI was set to true");
+      logger.info("Connecting to localhost database because CI was set to true");
       await mongoose.connect("mongodb://localhost:27017/test");
     } else if (ENVIRONMENT === "test" || ENVIRONMENT === "development") {
       logger.info(
         `Connecting to DEV database because env was set to: ${ENVIRONMENT}`
       );
-      if (!MONGODB_DEV_URI)
-        throw new Error(
-          "MONGODB_DEV_URI is not defined in .env file, and ENVIRONMENT was set to " +
-            ENVIRONMENT
-        );
       await mongoose.connect(MONGODB_DEV_URI);
     } else if (ENVIRONMENT === "production") {
       logger.info("Connecting to production database");
       await mongoose.connect(MONGODB_URI);
       logger.info("Successfully connected to production database");
     } else
-      throw new Error(
-        "ENVIRONMENT was set to " +
-          ENVIRONMENT +
-          " which is not a valid value. Valid values are: test, development, production, and ci"
-      );
+      throw new Error(`ENVIRONMENT was set to ${ENVIRONMENT} which is not a valid value. Valid values are: test, development, production, and ci`);
   } catch (error) {
     logger.error("Connection to database failed. Error below");
     logger.error(error);
