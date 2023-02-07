00662dd47477b545cfb8a54019b4238dc8e95e1f
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-07 18:44:39 +0200

    added conneting to cache

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import { connectToDatabase, disconnectFromDatabase } from "./utils/db";
+import { connectToCache, disconnectFromCache } from "./utils/redis";
 import { PORT } from "./utils/config";
 
 import app from "./app";
 import { logger } from "./utils/logger";
 
-connectToDatabase();
-
+(async () => {
+  logger.info("Establishing connections...");
+  await connectToDatabase();
+  await connectToCache();
+  logger.info("Connections established");
+})();
 
 app.listen(PORT, () => {
   logger.info(`Server listening on http://localhost:${PORT}`);
@@ -14,5 +19,6 @@ app.listen(PORT, () => {
 process.on("SIGINT", async () => {
   logger.info("SIGINT signal received. Shutting down gracefully");
   await disconnectFromDatabase();
+  await disconnectFromCache();
   process.exit(0);
 });
