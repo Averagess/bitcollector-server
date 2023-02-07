989ffb22c546232903766e17662761a233c831f5
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-07 18:45:40 +0200

    created updatePlayer

---
+++ src/datafetchers/updatePlayer.ts
@@ -0,0 +1,24 @@
+import { Player } from "../models";
+import { PlayerInterface } from "../types";
+import { client } from "../utils/redis";
+import { logger } from "../utils/logger";
+
+const updatePlayer = async (player: PlayerInterface, timestamps: boolean): Promise<PlayerInterface | null> => {
+  // First we need to find the player in the database
+  const updatedPlayer = await Player.findOneAndUpdate({ discordId: player.discordId }, { ...player }, { new: true, timestamps });
+
+  // If the player exists in the database, we return it and save it in cache
+  if(updatedPlayer) {
+    logger.info("Player saved in database, updating cache...");
+    client.set(updatedPlayer.discordId, JSON.stringify(updatedPlayer));
+    return updatedPlayer;
+  }
+  else {
+    // If the player doesn't exist in the database, we return null
+    logger.error("Player not found in database, returning null...");
+    return null;
+  }
+
+};
+
+export default updatePlayer;
