d14e70027f567f5420af5e5bf894a95302aa8db5
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-08 17:53:51 +0200

    serves public now

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(cors());
 app.use(httpConsoleLogger);
 app.use(fileLogger);
+app.use(express.static("public"));
 app.use("/login", loginRouter); // Login route is not protected by authenticator
 
 if(ENVIRONMENT === "development") {
