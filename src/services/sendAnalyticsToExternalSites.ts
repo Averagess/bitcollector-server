1693e19815188eea2d5df178b9c029048b4a2453
Averagess <45748163+Averagess@users.noreply.github.com>
2023-02-16 20:34:23 +0200

    created services for sending analytics

---
+++ src/services/sendAnalyticsToExternalSites.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+import { BOT_ID, TOPGG_TOKEN, DISCORDBOTLIST_TOKEN, DISCORDS_TOKEN } from "../utils/config";
+import { logger } from "../utils/logger";
+
+export const sendAnalyticsToTopGG = async (server_count: number) => {
+  const headers = {
+    Authorization: TOPGG_TOKEN
+  };
+  try {
+    logger.info("POST:ing analytics to top.gg");
+    const { status } = await axios.post(`https://top.gg/api/bots/${BOT_ID}/stats`, { server_count }, { headers });
+    logger.info(`Successfully POST:ed analytics to top.gg with status code ${status}`);
+  } catch (error) {
+    logger.error("Failed to POST analytics to top.gg, error below");
+    logger.error(error);
+  }
+};
+
+export const sendAnalyticsToDiscordBotList = async (server_count: number) => {
+  const headers = {
+    Authorization: DISCORDBOTLIST_TOKEN
+  };
+
+  try {
+    logger.info("POST:ing analytics to discordbotlist.com");
+    const { status } = await axios.post(`https://discordbotlist.com/api/v1/bots/${BOT_ID}/stats`, { guilds: server_count }, { headers });
+    logger.info(`Successfully POST:ed analytics to discordbotlist.com with status code ${status}`);
+  } catch (error) {
+    logger.error("Failed to POST analytics to discordbotlist.com, error below");
+    logger.error(error);
+  }
+};
+
+export const sendAnalyticsToDiscords = async (server_count: number) => {
+  const headers = {
+    Authorization: DISCORDS_TOKEN
+  };
+
+  try {
+    logger.info("POST:ing analytics to discords.com");
+    const { status } = await axios.post(`https://discords.com/bots/api/bot/${BOT_ID}`, { server_count }, { headers });
+    logger.info(`Successfully POST:ed analytics to discords.com with status code ${status}`);
+  } catch (error) {
+    logger.error("Failed to POST analytics to discords.com, error below");
+    logger.error(error);
+  }
+};
