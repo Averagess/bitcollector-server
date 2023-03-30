7d39fc94b0e63917b855b6edebb4c6d963ecb2bb
Averagess <45748163+Averagess@users.noreply.github.com>
2023-03-30 17:06:34 +0300

    formatted

--- src/routes/putters.ts
+++ src/routes/putters.ts
@@ -1,22 +1,27 @@
 import { Router } from "express";
-import { client } from "../utils/redis";
 const router = Router();
 
+import { client } from "../utils/redis";
 import { Player } from "../models";
 
-router.put("/updatePlayer", async (req,res) => {
+router.put("/updatePlayer", async (req, res) => {
   const body = req.body;
 
-  if(!body.discordId || Object.keys(body).length === 0) return res.status(400).json({ error: "Missing or invalid body" });
+  if (!body.discordId)
+    return res.status(400).json({ error: "Missing or invalid body" });
 
   try {
-    const player = await Player.findOneAndUpdate({ discordId: body.discordId }, body, { new: true });
-    if(!player) return res.status(404).json({ error: "Player not found" });
+    const player = await Player.findOneAndUpdate(
+      { discordId: body.discordId },
+      body,
+      { new: true }
+    );
+    if (!player) return res.status(404).json({ error: "Player not found" });
 
     await client.set(body.discordId, JSON.stringify(player.toJSON()));
     return res.status(200).json({ message: "Player updated" });
   } catch (error) {
-    if(error instanceof Error){
+    if (error instanceof Error) {
       return res.status(400).json({ error: error.message });
     }
   }
