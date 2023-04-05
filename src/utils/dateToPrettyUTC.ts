c533bffece8889f62979de8b25603494bfe33c6a
Averagess <45748163+Averagess@users.noreply.github.com>
2023-04-05 09:32:32 +0300

    slight update to formatting, also fixed bug

--- src/utils/dateToPrettyUTC.ts
+++ src/utils/dateToPrettyUTC.ts
@@ -5,7 +5,7 @@ export default function dateToPrettyUTC(date: Date): string {
   const minutes = utc.split(" ")[4].split(":")[1];
 
   const day = utc.split(" ")[1];
-  const month = date.getMonth();
+  const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
 
   return `${day}.${month} ${hours}:${minutes} GMT`;
 }
